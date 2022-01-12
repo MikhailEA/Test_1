d58266405f2cec3577c8567263525528946b663c
MikhailEA <michev.work@gmail.com>
2022-01-12 18:22:08 +0300

    Счетчик

--- app/src/main/java/com/michev_work/test_1/MainActivity.java
+++ app/src/main/java/com/michev_work/test_1/MainActivity.java
@@ -3,12 +3,25 @@ package com.michev_work.test_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+    private TextView tview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tview = findViewById(R.id.textView);
+    }
+
+    public void onClickCounter(View view) {
+        counter++;
+        tview.setText(String.valueOf(counter));
+
     }
 }
